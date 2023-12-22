744e608be6447bc63a97154fdc3a1721dc43adc1
Janindu <janindu12345@gmail.com>
2023-12-21 21:09:41 -0700

    added tons of stuff pt.2

--- gallery.js
+++ gallery.js
@@ -1,31 +1,29 @@
 window.addEventListener("load", () => {
   const overlay = document.getElementById("page-overlay");
 
-  // Start fading out the overlay
+  // fading out the overlay
   overlay.style.opacity = "0";
 
   // After the fade-out transition, remove the overlay and start the underline animation
   setTimeout(() => {
     overlay.style.display = "none";
 
-    // Select the element you want to animate
     const animatedElement = document.querySelector(".animated-underline");
 
-    // Add a class that starts the animation
     if (animatedElement) {
       animatedElement.classList.add("start-animation");
     }
-  }, 500); // This duration should match the CSS transition duration
+  }, 500); 
 });
 
 setTimeout(() => {
   overlay.style.display = "none";
 
-  // Add a slight delay before starting the animation
+  // slight delay before starting the animation
   setTimeout(() => {
     const animatedElement = document.querySelector(".animated-underline");
     if (animatedElement) {
       animatedElement.classList.add("start-animation");
     }
-  }, 100); // Slight delay to ensure re-render is complete
+  }, 100); 
 }, 500);
